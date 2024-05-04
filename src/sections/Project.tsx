6753353e5198a3708d3a92f017a9230245b39e92
duy-nd51 <duyndhk@coin98.finance>
2024-05-04 21:46:52 +0700

    refactor: update projects

--- src/sections/Project.tsx
+++ src/sections/Project.tsx
@@ -1,23 +1,35 @@
 import useProducts from "@/hook/useProducts";
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
+import { ClientRouting } from "@/constants/routing";
 
 const Projects = () => {
   const { products } = useProducts();
   return (
-    <div>
-      <div>
+    <div className="w-full container vertical-spacing">
+      <div className="min-h-[315px]">
         {products.slice(0, 1).map((product) => {
           return (
-            <div className="flex">
-              <div className="flex-1"></div>
+            <div className="flex h-full">
+              <div className="flex-1 relative">
+                <Image
+                  src={`${ClientRouting.imageURL}/bg.png`}
+                  alt="logo"
+                  className="w-full h-full top-0 left-0 absolute object-cover"
+                  width={324}
+                  height={174}
+                />
+              </div>
               <div className="flex-1">
                 <div>
                   <h3>{product.name}</h3>
                   <p>{product.description}</p>
                 </div>
 
-                <Link href={"/"} className="">Learn more</Link>
+                <Link href={`/product/${product.id}`} className="mt-auto">
+                  Learn more
+                </Link>
               </div>
             </div>
           );
