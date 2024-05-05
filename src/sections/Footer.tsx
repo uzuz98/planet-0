d9700253b3db20bc377ae1a8c7a5a2fa2de81122
duy-nd51 <duyndhk@coin98.finance>
2024-05-05 07:51:28 +0700

    style: footer comp

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -44,7 +44,7 @@ const data = [
 
 const Footer = () => {
   return (
-    <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 container py-[50px]">
+    <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 container py-[50px] gap-[20px]">
       <div>
         <Link aria-label="Logo" href="/">
           <Image
@@ -59,7 +59,7 @@ const Footer = () => {
       {data.map((menu) => {
         return (
           <div key={Math.random()} className="flex flex-col gap-[15px]">
-            <div className="font-medium">{menu.title}</div>
+            <div className="font-semibold">{menu.title}</div>
             {menu.subItems.map((item) => {
               return (
                 <div key={Math.random()}>
